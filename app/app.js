0b4950aa4ace6af3d55c6269aaf10a7e621bd1e4
MateuszKuriata <mateusz.kuriata@altimi.com>
2016-04-11 15:37:36 +0200

    array of buttons

--- app/app.js
+++ app/app.js
@@ -1,25 +1,57 @@
 var React = require('react');
 var ReactDom = require('react-dom');
+var ArticlesData = require('./ArticlesData.json');
 
-
+var Button = React.createClass({
+	
+	getInitialState: function() {
+    	return {liked: false};
+  	},
+  	handleClick: function(event) {
+    	this.setState({liked: !this.state.liked});
+    	//alert('aww yeah')
+ 	},
+	render : function(){
+		var text = this.state.liked ? 'like' : 'haven\'t liked';
+		var textButtton = this.state.liked ? 'unlike' : 'like!';
+		return(
+			<div>
+				<button type="button" onClick={this.handleClick} className="btn btn-primary">{this.props.title}</button>
+				<p>{text}</p>
+			</div>
+		)
+	}
+});
 var Main = React.createClass({
 	render : function(){
+	var Buttonas = [
+      "Some Article",
+      "Some Other Article",
+      "Yet Another Article",
+      "Still More",
+      "Some Article",
+      "Some Other Article",
+      "Yet Another Article",
+      "Still More",
+      "Some Article",
+      "Some Other Article",
+      "Yet Another Article",
+      "Still More",
+    ].map((title, i) => <Button key={i} type="button" index={i} title={title} /> );
+
+    console.log(ArticlesData);
+
+    Buttonas.push(<Button key={77} type="button" index={77} title={'adam lambert'} />);
+
 		return(
 			<div  className="jumbotron text-center">
 				<h1>Welcome on the ReactJS tutorial</h1>
+				{Buttonas}
 			</div>
 		)
 	}
 });
 
-var Button = React.createClass({
-	render : function(){
-		return(
-			<button type="button" className="btn btn-primary">Get started</button>
-		)
-	}
-});
 
-var ButtonsCollection = [Button,Button,Button];
 
 ReactDom.render(<Main/>,document.getElementById('app'))
